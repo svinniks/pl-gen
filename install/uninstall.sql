c5bef37cd405aa7e9bc36a0b5142d57e9468ffc4
Sergejs Vinniks <svinniks@inbox.lv>
2019-02-13 10:29:51 +0200

    Added installation scripts

---
+++ install/uninstall.sql
@@ -0,0 +1,24 @@
+DROP TRIGGER after_ddl
+/
+DROP TRIGGER before_ddl
+/
+
+DROP FUNCTION register
+/
+
+DROP PACKAGE synchronization
+/
+DROP PACKAGE generation
+/
+
+DROP TYPE t_generators
+/
+DROP TYPE t_generator_registration
+/
+DROP TYPE t_generator
+/
+
+DROP CONTEXT generation_global
+/
+DROP CONTEXT generation
+/
